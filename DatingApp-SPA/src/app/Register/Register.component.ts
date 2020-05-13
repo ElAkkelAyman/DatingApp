6923d6339f727c3dc088d72b46bed744b66476b8
unknown <iouazzi@SQLI-MAELAKKEL.SQLI>
2020-05-13 18:18:01 +0000

    Section 4

---
+++ DatingApp-SPA/src/app/Register/Register.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './Register.component.html',
+  styleUrls: ['./Register.component.css']
+})
+export class RegisterComponent implements OnInit {
+ @Input() valuesFromHome: any;
+ @Output() cancelRegister = new EventEmitter();
+model: any = {};
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  Register(){
+    this.authService.Register(this.model).subscribe(next => {
+      console.log('successfuly registered') ;
+     }, error => {
+      console.log(error);
+     });
+    console.log(this.model);
+  }
+  Cancel() {
+    this.cancelRegister.emit(false);
+    console.log('cancel register !');
+  }
+
+}
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -4,18 +4,27 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from './_services/auth.service';
+import { RegisterComponent } from './Register/Register.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      NavComponent,
+      RegisterComponent,
+      HomeComponent
    ],
    imports: [
-      BrowserModule,
-      HttpClientModule
+	 BrowserModule,
+	 HttpClientModule,
+	 FormsModule
+	],
+   providers: [
+      AuthService
    ],
-   providers: [],
    bootstrap: [
       AppComponent
    ]
